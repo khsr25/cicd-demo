e01b109919ea7a576198a07db2186ce1bbf1ffc1
khsr25 <102398532+khsr25@users.noreply.github.com>
2023-10-07 16:03:22 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from app import app
 
 @pytest.fixture
 def client():
-    app.config['TESTING'] = True
+    app.config['TESTING'] = False
     with app.test_client() as client:
         yield client
 
